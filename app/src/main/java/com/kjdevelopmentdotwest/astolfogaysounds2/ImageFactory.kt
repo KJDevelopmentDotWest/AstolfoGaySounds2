33e3fecb4d7615b397419849db3a4085d466399a
bring <bringonthepretty@gmail.com>
2021-02-08 23:51:30 +0300

    upd

--- app/src/main/java/com/kjdevelopmentdotwest/astolfogaysounds2/ImageFactory.kt
+++ app/src/main/java/com/kjdevelopmentdotwest/astolfogaysounds2/ImageFactory.kt
@@ -2,28 +2,42 @@ package com.kjdevelopmentdotwest.astolfogaysounds2
 
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.util.DisplayMetrics
+import android.view.Display
+
 
 class ImageFactory {
     companion object{
 
+        var imageWidth : Int = 0
+        var imageHeight : Int = 0
+        var displayMetrics : DisplayMetrics? = null
+
         var resultImage : Bitmap? = null
 
-        fun drawing(key : Bitmap) : Bitmap {
-
-            val bitmap = key.copy(Bitmap.Config.ARGB_8888, true)
-
-            val canvas = Canvas(bitmap)
-
-            canvas.drawARGB(255, 255, 255, 255)
-
-            return bitmap
+        private fun getRes(){
+            imageWidth = displayMetrics!!.widthPixels
+            imageHeight = displayMetrics!!.heightPixels - (24 + 50) * displayMetrics!!.density.toInt() // 24dp status bar 50dp bottom buttons
         }
 
-        fun mergeBitmaps(back : Bitmap, front : Bitmap, offsetLeft: Float, offsetTop: Float){
-            var result = Bitmap.createBitmap(1080, 1680, Bitmap.Config.ARGB_8888)
+        fun scaleBitmap(src : Bitmap){
+            getRes()
+
+            val result = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(result)
 
-            canvas.drawBitmap(Bitmap.createScaledBitmap(back, 1080, 1680, true), 0f, 0f, null)
+            canvas.drawBitmap(Bitmap.createScaledBitmap(src, imageWidth, imageHeight, true), 0f, 0f, null)
+
+            resultImage = result
+        }
+
+        fun mergeScaleBitmaps(back : Bitmap, front : Bitmap, offsetLeft : Float, offsetTop : Float){
+            getRes()
+
+            val result = Bitmap.createBitmap(imageWidth, imageHeight, Bitmap.Config.ARGB_8888)
+            val canvas = Canvas(result)
+
+            canvas.drawBitmap(Bitmap.createScaledBitmap(back, imageWidth, imageHeight, true), 0f, 0f, null)
             canvas.drawBitmap(Bitmap.createScaledBitmap(front, 300, 1000, true), offsetLeft, offsetTop, null)
 
             resultImage = result
--- app/src/main/java/com/kjdevelopmentdotwest/astolfogaysounds2/MainActivity.kt
+++ app/src/main/java/com/kjdevelopmentdotwest/astolfogaysounds2/MainActivity.kt
@@ -1,38 +1,67 @@
 package com.kjdevelopmentdotwest.astolfogaysounds2
 
+import android.content.Context
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  mainImage: ImageView
+    lateinit var shopButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainImage = findViewById<ImageView>(R.id.imageView)
-        val shopButton = findViewById<Button>(R.id.shopButton)
+        mainImage = findViewById<ImageView>(R.id.imageView)
+        shopButton = findViewById<Button>(R.id.shopButton)
 
+        setUp()
+    }
+
+    override fun onStart() {
+        super.onStart()
         if (ImageFactory.resultImage != null){
             mainImage.setImageBitmap(ImageFactory.resultImage)
         } else {
-            mainImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.astolfo))
+            ImageFactory.scaleBitmap(BitmapFactory.decodeResource(resources, R.drawable.astolfo))
+            mainImage.setImageBitmap(ImageFactory.resultImage)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ImageFactory.resultImage != null){
+            mainImage.setImageBitmap(ImageFactory.resultImage)
+        } else {
+            ImageFactory.scaleBitmap(BitmapFactory.decodeResource(resources, R.drawable.astolfo))
+            mainImage.setImageBitmap(ImageFactory.resultImage)
+        }
+    }
+
+    private fun setUp(){
+        val displayMetrics = DisplayMetrics()
+        val windowManager = applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        ImageFactory.displayMetrics = displayMetrics
 
         mainImage.setOnClickListener {
             View.OnClickListener {
-                ImageFactory.mergeBitmaps(BitmapFactory.decodeResource(resources, R.drawable.astolfo), BitmapFactory.decodeResource(resources, R.drawable.astolfoface), 100f, 50f)
+                ImageFactory.mergeScaleBitmaps(BitmapFactory.decodeResource(resources, R.drawable.astolfo), BitmapFactory.decodeResource(resources, R.drawable.astolfoface), 100f, 50f)
                 mainImage.setImageBitmap(ImageFactory.resultImage)
             }
         }
 
         shopButton.setOnClickListener{
-            ImageFactory.mergeBitmaps(BitmapFactory.decodeResource(resources, R.drawable.astolfo), BitmapFactory.decodeResource(resources, R.drawable.astolfoface), 100f, 50f)
+            ImageFactory.mergeScaleBitmaps(BitmapFactory.decodeResource(resources, R.drawable.astolfo), BitmapFactory.decodeResource(resources, R.drawable.astolfoface), 100f, 50f)
             mainImage.setImageBitmap(ImageFactory.resultImage)
         }
     }
